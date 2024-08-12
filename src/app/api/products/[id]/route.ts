29355a40137ef9bb78473f76106f82df5d477613
Abdullah <abdullahfaiz15@gmail.com>
2024-08-13 02:56:35 +0530

    test

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -6,10 +6,7 @@ type Params = {
 };
 
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-    // console.log(params)
     console.log(params.id)
-    // console.log(params.id)
-
       const product = await getProductById(params.id);
       return Response.json( product )
     
